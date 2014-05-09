3c3f85c8839926379b52f965c299e68e210ec975
srinivas31 <srinivas.srk@gmail.com>
2014-05-08 20:11:56 -0500

    Second Commit

--- plot4.R
+++ plot4.R
@@ -38,7 +38,7 @@ plot(initial$Time, initial$Voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(initial$Time, initial$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
 lines(initial$Time, initial$Sub_metering_2, type="l", col="red")
 lines(initial$Time, initial$Sub_metering_3, type="l", col="blue")
-legend("topright", lty=1, col = c("black","red", "blue"), 
+legend("topright", lty=1, bty="n", cex=0.9, col = c("black","red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3" ))
 
 
